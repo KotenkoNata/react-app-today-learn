ecf35f1dab936cba5874102a986d4abc1ccfa227
KotenkoNata <kotenkonata8@gmail.com>
2023-03-28 14:44:11 +0300

    feat: create react component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import './style.css';
 function App() {
   return (
-    <h1>Hello world</h1>
-  );
-}
+      <header className="header">
+    <div className="logo">
+      <img
+          src="./img/logo.png"
+          height="68"
+          width="68"
+          alt="Today I Learned Logo"
+      />
+      <h1>Today I Learned</h1>
+    </div>
+
+    <button className="btn btn-large btn-open">Share a fact</button>
+  </header>
+  )};
 
 export default App;
