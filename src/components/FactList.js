e228ca34204abfa2ed9b8d8421a379ef5923547c
KotenkoNata <kotenkonata8@gmail.com>
2023-03-29 16:46:57 +0300

    feat: render the list of facts

--- src/components/FactList.js
+++ src/components/FactList.js
@@ -1,5 +1,41 @@
-function FactList() {
-    return <section>Fact list</section>
+const CATEGORIES = [
+    { name: "technology", color: "#3b82f6" },
+    { name: "science", color: "#16a34a" },
+    { name: "finance", color: "#ef4444" },
+    { name: "society", color: "#eab308" },
+    { name: "entertainment", color: "#db2777" },
+    { name: "health", color: "#14b8a6" },
+    { name: "history", color: "#f97316" },
+    { name: "news", color: "#8b5cf6" },
+];
+
+function FactList({initialFacts}) {
+    const facts = initialFacts;
+    return <section>
+        <ul className="facts-list">
+            {facts.map(fact =>
+                <li key={fact.id} className="fact">
+                    <p>
+                        {fact.text}
+                        <a
+                            className="source"
+                            href={fact.source}
+                            target="_blank"
+                        >(Source)</a
+                        >
+                    </p>
+                    <span className="tag" style={{backgroundColor: CATEGORIES.find((item=>item.name === fact.category)).color}}
+                    >{fact.category}</span
+                    >
+                    <div className="vote-buttons">
+                        <button>👍 {fact.votesInteresting}</button>
+                        <button>🤯 {fact.votesMindblowing}</button>
+                        <button>⛔️ {fact.votesFalse}</button>
+                    </div>
+                </li>
+            )}
+        </ul>
+    </section>
 }
 
 export default FactList;
