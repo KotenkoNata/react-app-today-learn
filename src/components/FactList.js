6d7b92fa5fff02d98768caba6f26881baaf84a52
KotenkoNata <kotenkonata8@gmail.com>
2023-03-29 19:03:02 +0300

    feat: passing and receiving props

--- src/components/FactList.js
+++ src/components/FactList.js
@@ -1,40 +1,14 @@
-const CATEGORIES = [
-    { name: "technology", color: "#3b82f6" },
-    { name: "science", color: "#16a34a" },
-    { name: "finance", color: "#ef4444" },
-    { name: "society", color: "#eab308" },
-    { name: "entertainment", color: "#db2777" },
-    { name: "health", color: "#14b8a6" },
-    { name: "history", color: "#f97316" },
-    { name: "news", color: "#8b5cf6" },
-];
+import Fact from "./Fact";
 
 function FactList({initialFacts}) {
     const facts = initialFacts;
     return <section>
         <ul className="facts-list">
             {facts.map(fact =>
-                <li key={fact.id} className="fact">
-                    <p>
-                        {fact.text}
-                        <a
-                            className="source"
-                            href={fact.source}
-                            target="_blank"
-                        >(Source)</a
-                        >
-                    </p>
-                    <span className="tag" style={{backgroundColor: CATEGORIES.find((item=>item.name === fact.category)).color}}
-                    >{fact.category}</span
-                    >
-                    <div className="vote-buttons">
-                        <button>👍 {fact.votesInteresting}</button>
-                        <button>🤯 {fact.votesMindblowing}</button>
-                        <button>⛔️ {fact.votesFalse}</button>
-                    </div>
-                </li>
+                <Fact key = {fact.id} fact = {fact}/>
             )}
         </ul>
+        <p>There are {facts.length} facts in the database. Add your own</p>
     </section>
 }
 
