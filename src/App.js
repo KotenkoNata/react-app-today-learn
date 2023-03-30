4c5484b12b02e0aea1095d13e15898cc705d67de
KotenkoNata <kotenkonata8@gmail.com>
2023-03-30 16:48:58 +0300

    feat: displaying and hiding the form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import './style.css';
 import CategoryFilter from "./components/CategoryFilter";
 import NewFactForm from "./components/NewFactForm";
 import FactList from "./components/FactList";
-import Counter from "./components/Counter";
+import {useState} from "react";
+
 
 function App() {
+    const [showForm, setShowForm] = useState(false);
     const appTitle = 'Today I Learned';
+
   return (
       <>
           <header className="header">
@@ -18,10 +21,11 @@ function App() {
               />
               <h1>{appTitle}</h1>
             </div>
-            <button className="btn btn-large btn-open">Share a fact</button>
+            <button
+                className="btn btn-large btn-open"
+                onClick={()=>setShowForm(()=>!showForm)}>Share a fact</button>
           </header>
-          <Counter></Counter>
-          <NewFactForm />
+          {showForm ? <NewFactForm/> : null}
           <main className='main'>
               <CategoryFilter />
               <FactList></FactList>
