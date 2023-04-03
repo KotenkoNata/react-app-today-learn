981416e3711bdc5e79a07cd00ace602da4b29cd3
KotenkoNata <kotenkonata8@gmail.com>
2023-04-03 16:35:27 +0300

    feat: load data from supabase with useEffect

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import './style.css';
 import CategoryFilter from "./components/CategoryFilter";
 import NewFactForm from "./components/NewFactForm";
 import FactList from "./components/FactList";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Header from "./components/Header";
 import {initialFacts} from "./utils/data";
+import supabase from "./supabase";
 
 
 function App() {
     const [showForm, setShowForm] = useState(false);
-    const [facts, setFacts] = useState(initialFacts);
+    const [facts, setFacts] = useState([]);
+
+    useEffect(() => {
+
+        async function getFacts(){
+            const { data: facts, error } = await supabase
+                .from('facts')
+                .select('*')
+            setFacts(facts);
+        }
+
+        getFacts();
+
+    }, [])
+
 
   return (
       <>
